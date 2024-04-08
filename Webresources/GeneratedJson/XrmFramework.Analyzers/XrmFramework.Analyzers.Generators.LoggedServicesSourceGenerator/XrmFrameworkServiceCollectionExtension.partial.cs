dad673241bec1244eb6f9b005c6559efb9f92bf1
Ourvouai Louan <l.ourvouai@dimsi.fr>
2024-04-08 16:47:29 +0200

    CHanger de dossier et d'extension de fichier

---
+++ Webresources/GeneratedJson/XrmFramework.Analyzers/XrmFramework.Analyzers.Generators.LoggedServicesSourceGenerator/XrmFrameworkServiceCollectionExtension.partial.cs
@@ -0,0 +1,25 @@
+﻿#if PLUGIN || CORE_PROJECT
+using BoDi;
+
+namespace XrmFramework
+{
+    partial class InternalDependencyProvider
+    {
+        static partial void RegisterServices(IObjectContainer container)
+        {
+        }
+    }
+}
+#endif
+#if CORE_PROJECT
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    partial class XrmFrameworkServiceCollectionExtension
+    {
+        static partial void RegisterServices(IServiceCollection serviceCollection)
+        {
+        }
+    }
+}
+#endif
