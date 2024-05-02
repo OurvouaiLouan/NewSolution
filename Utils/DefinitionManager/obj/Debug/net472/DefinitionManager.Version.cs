001beff4fb4d27549c56731034afb541fe2bb044
Ourvouai Louan <l.ourvouai@dimsi.fr>
2024-05-02 16:28:18 +0200

    Mis en place création de json.cs via les .table de NewSolution

--- Utils/DefinitionManager/obj/Debug/net472/DefinitionManager.Version.cs
+++ Utils/DefinitionManager/obj/Debug/net472/DefinitionManager.Version.cs
@@ -11,8 +11,8 @@
 #pragma warning disable CA2243
 
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.7.62255")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.7+2ff334917d")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.8.57435")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.8+5be0e647b1")]
 #if NETSTANDARD || NETFRAMEWORK || NETCOREAPP
 [System.CodeDom.Compiler.GeneratedCode("Nerdbank.GitVersioning.Tasks","3.4.255.64262")]
 #endif
@@ -21,13 +21,13 @@
 #endif
 internal static partial class ThisAssembly {
     internal const string AssemblyConfiguration = "Debug";
-    internal const string AssemblyFileVersion = "1.0.7.62255";
-    internal const string AssemblyInformationalVersion = "1.0.7+2ff334917d";
+    internal const string AssemblyFileVersion = "1.0.8.57435";
+    internal const string AssemblyInformationalVersion = "1.0.8+5be0e647b1";
     internal const string AssemblyName = "DefinitionManager";
     internal const string AssemblyTitle = "DefinitionManager";
     internal const string AssemblyVersion = "1.0.0.0";
-    internal static readonly System.DateTime GitCommitDate = new System.DateTime(638495657380000000L, System.DateTimeKind.Utc);
-    internal const string GitCommitId = "2ff334917d741970829a55b9afcb71e749d2859d";
+    internal static readonly System.DateTime GitCommitDate = new System.DateTime(638497384390000000L, System.DateTimeKind.Utc);
+    internal const string GitCommitId = "5be0e647b1824888df9d04685a50530a599434bc";
     internal const bool IsPrerelease = false;
     internal const bool IsPublicRelease = false;
     internal const string PublicKey = "00240000048000009400000006020000002400005253413100040000010001000d196816c56c09f53b4235803db7e452f0c1911a84b0f48ed49fc5b6cd544869a7e74fb971f388bd335b537b22e43a63101907a395e40bc0e434dc9a98c8f2d4e61e84f274cbf9bcb2b8415f582b26d5f2bd3d152d1736440ecd978b8216bb9a6ee429c9f84e87b00ca4e8fb747292d433a4017c8fa51456e80c6f12c95f59b4";
