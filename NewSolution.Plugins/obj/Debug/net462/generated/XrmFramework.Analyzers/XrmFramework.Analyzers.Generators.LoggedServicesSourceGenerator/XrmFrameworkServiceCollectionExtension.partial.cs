a14780bf93ea4395b458c60e46814952d3b1092b
Ourvouai Louan <l.ourvouai@dimsi.fr>
2024-05-22 16:30:02 +0200

    convertion des json.cs en js (en cours)

---
+++ NewSolution.Plugins/obj/Debug/net462/generated/XrmFramework.Analyzers/XrmFramework.Analyzers.Generators.LoggedServicesSourceGenerator/XrmFrameworkServiceCollectionExtension.partial.cs
@@ -0,0 +1,30 @@
+﻿#if PLUGIN || CORE_PROJECT
+using BoDi;
+using XrmFramework;
+
+namespace XrmFramework
+{
+    partial class InternalDependencyProvider
+    {
+        static partial void RegisterServices(IObjectContainer container)
+        {
+            RegisterService<IService, DefaultService, LoggedIService>(container);
+
+        }
+    }
+}
+#endif
+#if CORE_PROJECT
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    partial class XrmFrameworkServiceCollectionExtension
+    {
+        static partial void RegisterServices(IServiceCollection serviceCollection)
+        {
+            RegisterService<IService>(serviceCollection);
+
+        }
+    }
+}
+#endif
